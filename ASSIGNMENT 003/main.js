324f8842623d0b4ad556534ecb1f909d52910b61
SalmanSarwarRajput <sarwarsalman92@gmail.com>
2024-03-01 18:52:45 +0500

    first commit

---
+++ ASSIGNMENT 003/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and its author
+let quote = "Nothing is impossible because impossible itself  I M POSSIBLE";
+let author = "Salman Sarwar";
+//printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ ASSIGNMENT 003/main.ts
@@ -0,0 +1,5 @@
+//storing the famous quote and its author
+let quote:string = "Nothing is impossible because impossible itself  I M POSSIBLE";
+let author:string = "Salman Sarwar";
+//printing the message
+console.log(`"${quote}" - ${author}`);
